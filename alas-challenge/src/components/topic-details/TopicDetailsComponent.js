dbaac6c484305d7aadf0c2a6fc7448d0751ea86c
Kristijan Bujak <cileb411@gmail.com>
2023-04-25 18:04:42 +0200

    kada se pritisne na topic iz tabele, prikazuju se u komponenti sa desne strane volume i sentiment vrednosti, za positive je zelena boja vrednosti, dok je za negativne brojeve crvena. Ako ne postoji sentiment, nece se ni prikazati.

--- alas-challenge/src/components/topic-details/TopicDetailsComponent.js
+++ alas-challenge/src/components/topic-details/TopicDetailsComponent.js
@@ -9,6 +9,23 @@ const TopicDetailsComponent = (props) => {
     <div className='topic-details-container'>
         {selectedTopic != undefined && <div className='topic-title'>Information on topic: {selectedTopic.label}</div>}
         {selectedTopic != undefined && <div className='topic-total-mentions'>Total mentions: {selectedTopic.volume}</div>}
+        <div className='sentiment-main-container'>
+        {selectedTopic != undefined && selectedTopic.sentiment.positive != undefined && selectedTopic.sentiment.positive != null 
+            && <div className='sentiment-container'>
+                    <label className='label-sentiment'>Positive: </label>
+                    <div className='div-positive-sentiment'>{selectedTopic.sentiment.positive}</div>
+                </div>}
+        {selectedTopic != undefined && selectedTopic.sentiment.neutral != undefined && selectedTopic.sentiment.neutral != null 
+            && <div className='sentiment-container'>
+                    <label className='label-sentiment'>Neutral: </label>
+                    <div className='div-neutral-sentiment'>{selectedTopic.sentiment.neutral}</div>
+                </div>}
+        {selectedTopic != undefined && selectedTopic.sentiment.negative != undefined && selectedTopic.sentiment.negative != null 
+            && <div className='sentiment-container'>
+                    <label className='label-sentiment'>Negative: </label>
+                    <div className='div-negative-sentiment'>{selectedTopic.sentiment.negative}</div>
+                </div>}
+        </div>
     </div>
   )
 }
