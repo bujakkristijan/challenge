d3c562f2a40aceddc25d26d21ffa538bdc97c0e4
Kristijan Bujak <cileb411@gmail.com>
2023-04-25 14:14:48 +0200

    dodat navbar koji u sustini ima samo naslov aplikacije, dodat background image na komponentama, bootstrap instaliran

--- alas-challenge/src/App.js
+++ alas-challenge/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavbarComponent from './components/navbar/NavbarComponent';
 import ListTopicComponent from './components/list-topic/ListTopicComponent';
-import data from './data/topics.json';
 
 function App() {
   return (
--- alas-challenge/src/components/navbar/NavbarComponent.js
+++ alas-challenge/src/components/navbar/NavbarComponent.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './NavbarComponent.css'
 
 const NavbarComponent = () => {
   return (
-    <div>NavbarComponent</div>
+    <>
+    <div className='navbar-container'>
+      <div className='title'>ALAS - challenge</div>
+    </div>
+    <div className='nav-bottom-line'></div>
+    </>
   )
 }
 
