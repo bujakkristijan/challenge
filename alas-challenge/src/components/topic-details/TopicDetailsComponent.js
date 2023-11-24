d0f5243bf8c34ebaf403aa8d5e482dba83bca4d7
Kristijan Bujak <kbujak996@gmail.com>
2023-11-24 01:09:59 +0100

    izbrisan nepotreban kod i importovan font

--- alas-challenge/src/components/topic-details/TopicDetailsComponent.js
+++ alas-challenge/src/components/topic-details/TopicDetailsComponent.js
@@ -7,27 +7,28 @@ const TopicDetailsComponent = (props) => {
     
   return (
     <div className='topic-details-container'>
-        {(selectedTopic == undefined || selectedTopic == undefined) && <div className='topic-title'>Select topic from table to see results</div>}
+        {/* null == undefined // true */}
+        {(selectedTopic == undefined) && <div className='topic-title'>Select topic from table to see results</div>}
         {selectedTopic != undefined && <div className='topic-title'>Information on topic: {selectedTopic.label}</div>}
         {selectedTopic != undefined && <div className='topic-total-mentions'>Total mentions: {selectedTopic.volume}</div>}
         <div className='sentiment-main-container'>
             {selectedTopic != undefined 
                 && <div className='sentiment-container'>
                         <label className='label-sentiment'>Positive: </label>
-                        {selectedTopic.sentiment.positive != undefined && selectedTopic.sentiment.positive != null && <div className='div-positive-sentiment'>{selectedTopic.sentiment.positive}</div>}
-                        {(selectedTopic.sentiment.positive == undefined || selectedTopic.sentiment.positive == null) && <div className='div-positive-sentiment'>0</div>}
+                        {selectedTopic.sentiment.positive != undefined && <div className='div-positive-sentiment'>{selectedTopic.sentiment.positive}</div>}
+                        {selectedTopic.sentiment.positive == undefined && <div className='div-positive-sentiment'>0</div>}
                     </div>}
             {selectedTopic != undefined 
                 && <div className='sentiment-container'>
                         <label className='label-sentiment'>Neutral: </label>
-                        {selectedTopic.sentiment.neutral != undefined && selectedTopic.sentiment.neutral != null && <div className='div-neutral-sentiment'>{selectedTopic.sentiment.neutral}</div>}
-                        {(selectedTopic.sentiment.neutral == undefined || selectedTopic.sentiment.neutral == null) && <div className='div-neutral-sentiment'>0</div>}
+                        {selectedTopic.sentiment.neutral != undefined && <div className='div-neutral-sentiment'>{selectedTopic.sentiment.neutral}</div>}
+                        {selectedTopic.sentiment.neutral == undefined && <div className='div-neutral-sentiment'>0</div>}
                     </div>}
             {selectedTopic != undefined
                 && <div className='sentiment-container'>
                         <label className='label-sentiment'>Negative: </label>
-                        {selectedTopic.sentiment.negative != undefined && selectedTopic.sentiment.negative != null && <div className='div-negative-sentiment'>{selectedTopic.sentiment.negative}</div>}
-                        {(selectedTopic.sentiment.negative == undefined || selectedTopic.sentiment.negative == null) && <div className='div-negative-sentiment'>0</div>}
+                        {selectedTopic.sentiment.negative != undefined && <div className='div-negative-sentiment'>{selectedTopic.sentiment.negative}</div>}
+                        {selectedTopic.sentiment.negative == undefined && <div className='div-negative-sentiment'>0</div>}
 
                     </div>}
         </div>
